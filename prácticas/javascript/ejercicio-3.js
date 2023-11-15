e0b44860c5b6d8b41f78fe52914b205469beeaeb
NicoLoreto <nicoloreto.dev@gmail.com>
2023-11-14 22:50:11 -0300

    Remove unused variables

--- prácticas/javascript/ejercicio-3.js
+++ prácticas/javascript/ejercicio-3.js
@@ -4,21 +4,19 @@ function calcularDiasCrecimiento(
   alturaDeseada
 ) {
   let crecimiento = velocidadCrecimiento - velocidadDecrecimiento;
-  let alturaActual = 0;
-  let dias = 0;
 
   if (velocidadCrecimiento <= 0 || velocidadDecrecimiento <= 0) {
-    return "Los valores de crecimiento y decrecimiento no pueden ser negativos o iguales a 0";
+    return 'Los valores de crecimiento y decrecimiento no pueden ser negativos o iguales a 0';
   } else if (velocidadCrecimiento === velocidadDecrecimiento) {
-    return "Así la planta nunca crecerá!";
+    return 'Así la planta nunca crecerá!';
   } else if (velocidadCrecimiento < velocidadDecrecimiento) {
-    return "La planta no puede decrecer más de lo que crecerá";
+    return 'La planta no puede decrecer más de lo que crecerá';
   } else {
     const calcularCrecimiento = () => {
       return alturaDeseada / crecimiento;
     };
 
-    return "La planta tardará en crecer " + calcularCrecimiento() + " días";
+    return 'La planta tardará en crecer ' + calcularCrecimiento() + ' días';
   }
 }
 
