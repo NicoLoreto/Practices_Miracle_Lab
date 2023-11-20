c44e747205b9cb1bb05453d1123f3dd83236a924
NicoLoreto <nicoloreto.dev@gmail.com>
2023-11-19 23:02:55 -0300

    Exercise-2 corrected

--- prácticas/typescript/ejercicios/src/ejercicio-2.ts
+++ prácticas/typescript/ejercicios/src/ejercicio-2.ts
@@ -1,28 +1,35 @@
 // Code Breaker
-
-function game(): string {
+function game() {
     const numSecret = "2561";
-
-    let arr = "";
-
-    let cruz = "";
-
-    let guion = "";
-
-    while (arr.length < 4) {
-        const randomNumber: number = Math.random() * 6;
-        const int = Math.floor(randomNumber);
-        const s = int.toString();
-        arr += s;
+    let randomNumber: string = "";
+    let crosses: string = "";
+    let dashes: string = "";
+    while (randomNumber.length < 4) {
+        const number = Math.floor(Math.random() * 6);
+        const numberToString = number.toString();
+        randomNumber += numberToString;
     }
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === numSecret[i]) {
-            cruz += "X";
+    console.log("The random number is:", randomNumber);
+
+    // Remove repeated elements.
+    const newSet = new Set(randomNumber);
+    const arrRandomNumbers = [...newSet];
+
+    // Save in an array the elements that have passed the condition for "X".
+    // This way, they are not considered in future iterations
+    const touched = [];
+
+    for (let i = 0; i < arrRandomNumbers.length; i++) {
+        if (arrRandomNumbers[i] === numSecret[i]) {
+            crosses += "X";
+            touched.push(arrRandomNumbers[i]);
         }
-        if (numSecret.includes(arr[i])) guion += "-";
+        if (numSecret.includes(arrRandomNumbers[i]) && !touched.includes(arrRandomNumbers[i])) {
+            dashes += "-";
+        }
     }
-    return cruz + guion;
-}
 
-console.log(game());
+    return crosses + dashes;
+}
+console.log("Result:", game());
