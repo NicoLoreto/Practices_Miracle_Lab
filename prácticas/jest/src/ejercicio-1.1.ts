41a421445c2de070f60641c52fc2c4dc1f543d64
NicoLoreto <nicoloreto.dev@gmail.com>
2023-11-20 14:30:17 -0300

    exercise 1.1 corrected

--- prácticas/jest/src/ejercicio-1.1.ts
+++ prácticas/jest/src/ejercicio-1.1.ts
@@ -1,30 +1,33 @@
 export function fizzBuzz(num: number): string {
-
-    switch (true) {
-
-        case typeof num === 'undefined':
-            return 'Debe ingresar un valor';
-
-        case num === 0:
-            return 'El valor ingresado no puede ser igual a cero';
-
-        case typeof num === 'string':
-            return 'La función no permite cadenas de texto';
-
-        case typeof num != 'number':
-            return 'El valor ingresado debe ser un número';
-
-        case num % 3 === 0 && num % 5 === 0:
-            return 'FizzBuzz';
-
-        case num % 3 === 0:
-            return 'Fizz';
-
-        case num % 5 === 0:
-            return 'Buzz';
-
-        default:
-            return num.toString();
+  
+    if (typeof num === 'undefined') {
+      return 'Debe ingresar un valor';
     }
-
+  
+    if (num === 0) {
+      return 'El valor ingresado no puede ser igual a cero';
+    }
+  
+    if (typeof num === 'string') {
+      return 'La función no permite cadenas de texto';
+    }
+  
+    if (typeof num !== 'number') {
+      return 'El valor ingresado debe ser un número';
+    }
+  
+    if (num % 3 === 0 && num % 5 === 0) {
+      return 'FizzBuzz';
+    }
+  
+    if (num % 3 === 0) {
+      return 'Fizz';
+    }
+  
+    if (num % 5 === 0) {
+      return 'Buzz';
+    } else {
+      return num.toString();
+    }
 }
+
