a920f701f3f6ed141b935becdf6a9db55a27a4e6
NicoLoreto <nicoloreto.dev@gmail.com>
2023-11-20 18:04:12 -0300

    Exercise-1.2.ts fixed

--- prácticas/jest/src/ejercicio-1.2.ts
+++ prácticas/jest/src/ejercicio-1.2.ts
@@ -1,14 +1,15 @@
 export function isLeapYear(year: number): boolean {
+  if (year <= 0) return false;
 
-    switch (year > 0) {
-        case year % 100 === 0 && year % 400 != 0:
-            return false;
-        case year % 4 === 0:
-            return true;
-        default:
-            return false;
-    }
+  if (year % 100 === 0 && year % 400 != 0) {
+    return false;
+  }
 
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 isLeapYear(2000);
